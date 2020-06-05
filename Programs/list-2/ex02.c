2f2df4fd6950312255d631aa315be1b613c6e5ac
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-05 20:04:42 -0300

    ex002
    fiz o ex002

---
+++ Programs/list-2/ex02.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+//VARIBLE
+    char name[30];
+    float fixed_salary, sales_month,salary;
+//INPUT
+    printf("Input your name: ");
+    gets(name);
+    printf("Fixed salary: ");
+    scanf("%f", &fixed_salary);
+    printf("Sales in the month: ");
+    scanf("%f", &sales_month);
+//MATH
+    salary = fixed_salary + (sales_month * 0.15);
+//OUTPUT
+    printf("\nHello %s \nFixed Salary:$%0.2f \nSales percentages:$%0.2f \nTotal Salary:$%0.2f \n",name, fixed_salary, sales_month, salary);
+
+    system("PAUSE");
+}
