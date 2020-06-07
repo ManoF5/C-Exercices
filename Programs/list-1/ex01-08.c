b888a8d008ddcac4fb38c0332c78b5ad1d2d57bc
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-07 18:14:29 -0300

    new ex.

---
+++ Programs/list-1/ex01-08.c
@@ -0,0 +1,24 @@
+/* Do a program that read a number from 1 to 5 and print the number in words. 
+if the user types a number different of this(1-5),
+print the message: invalid number */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+//VARIBLES
+    int number;
+//INPUT
+    printf("Type a number(1-5):");
+    scanf("%d", &number);
+//OUTPUT
+    switch(number){
+        case 1  : printf("One \n");            break;
+        case 2  : printf("Two \n");            break;
+        case 3  : printf("Three \n");          break;
+        case 4  : printf("Four \n");           break;
+        case 5  : printf("Five \n");           break;
+        default : printf("INVALID NUMBER \n"); break;
+    }
+    system("PAUSE");
+}
--- Programs/list-1/ex1-07.c
+++ Programs/list-1/ex1-07.c
@@ -4,3 +4,22 @@ else, tell which ones the highest number. */
 
 #include<stdio.h>
 #include<stdlib.h>
+
+main(){
+//VARIBLES
+    int n1, n2;
+//INPUT
+    printf("Type two numbers: ");
+    scanf("%d %d", &n1, &n2);
+//OUTPUT
+    if(n1 == n2){
+        printf("You type the same number: %d \n",n1);
+    }else{
+        if(n1 > n2){
+            printf("%d > %d \n", n1, n2);
+        }else{
+            printf("%d > %d \n", n2, n1);
+        }
+    }
+    system("PAUSE");
+}
