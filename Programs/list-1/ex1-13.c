7728aa108579adda92d20385f8c0900af7455a94
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-10 17:46:43 -0300

    Update ex1-13.c

--- Programs/list-1/ex1-13.c
+++ Programs/list-1/ex1-13.c
@@ -13,23 +13,30 @@ main(){
     int class, level;
 //INPUT
     printf("Good morning teacher! \n");
-    printf("Type your number of hours worked: ");
+    printf("Type your number of hours worked in the month: ");
     scanf("%f", &hours);
-    printf("");
+    printf("Type your number of classes in the month: ");
+    scanf("%d", &class);
     printf("Type your level: ");
     scanf("%d", &level);
 //OUTPUT
     switch(level){
         case 1 :{
-            
+            salary = 12 * (hours/class);
+            printf("Teacher this is your salary:%0.2f ",salary);
+            break;
         }
         case 2 :{
-
+            salary = 17 * (hours/class);
+            printf("Teacher this is your salary:%0.2f ",salary);
+            break;
         }
         case 3 :{
-
+            salary = 25 * (hours/class);
+            printf("Teacher this is your salary:%0.2f ",salary);
+            break;
         }
-        default : 
+        default : printf("Level invalid!");
     }
     system("PAUSE");
 }
