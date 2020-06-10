564e9664e856fd39b72e317691445d5b0ba805d5
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-10 16:13:09 -0300

    Create ex1-13.c

---
+++ Programs/list-1/ex1-13.c
@@ -0,0 +1,35 @@
+/* The school "APRENDER" pay your teachers per hour/class.
+Do a program than calculating and display the salary of the teacher.
+Level 1 teacher: $12.00 per hour/class
+Level 2 teacher: $17.00 per hour/class
+Level 3 teacher: $25.00 per hour/class */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+//VARIBLES
+    float hours, salary;
+    int class, level;
+//INPUT
+    printf("Good morning teacher! \n");
+    printf("Type your number of hours worked: ");
+    scanf("%f", &hours);
+    printf("");
+    printf("Type your level: ");
+    scanf("%d", &level);
+//OUTPUT
+    switch(level){
+        case 1 :{
+            
+        }
+        case 2 :{
+
+        }
+        case 3 :{
+
+        }
+        default : 
+    }
+    system("PAUSE");
+}
