ac61bf2d3f2912a94edd257704f50f917c3568b6
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-14 16:25:12 -0300

    Update ex2-11.c

--- Programs/list-2/ex2-11.c
+++ Programs/list-2/ex2-11.c
@@ -2,7 +2,7 @@
 a) The lowest height in the group
 b) The average women's height
 c) The number of mans
-d) The sex of the highest person  */
+d) The sex of the highest person  
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -10,11 +10,39 @@ d) The sex of the highest person  */
 
 main(){
 //VARIBLES
-    float height[15], lo_height = 1000, av_height;
-    char sex[15], hi_sex;
-    int i, num_mans;
+    float height, lo_height = 1000, hi_height = 0, avw_height = 0;
+    char sex, hi_sex[10];
+    int i = 0, woman = 0, num_mans = 0;
 //INPUT
-    
+    do{
+        printf("%d-Type the height and the sex(M or F): ",(i+1));
+        scanf("%f %c", &height, &sex);
+        //QUESTIONS 
+        if(height < lo_height){         // A
+            lo_height = height; 
+        }
+        if(sex == 'f' || sex == 'F'){   // B
+            avw_height += height;
+            woman++;
+        }
+        if(sex == 'm' || sex == 'M'){   // C
+            num_mans++;
+        }
+        if(height > hi_height){         // D
+            strcpy(hi_sex,sex);
+        }
+        i++;
+    }while(i < 4);
+    if(hi_sex == 'f' || hi_sex == 'F'){
+        strcpy(hi_sex,"Female");
+    }else if(hi_sex == 'm' || hi_sex == 'M'){
+        strcpy(hi_sex,"Male");
+    }
 //OUTPUT
-
+    printf("\nThe lowest height in the group:%0.2f \n", lo_height);
+    printf("The average women's height:%0.2f \n", (avw_height / woman));
+    printf("The number of mans:%d \n", num_mans);
+    printf("The sex of the highest person:%c \n", hi_sex);
 }
+
+*/
