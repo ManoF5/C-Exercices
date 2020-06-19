4b33790806aca1aa391a24a0a837aa49e54b6f65
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-19 11:50:54 -0300

    restart the ex3-01
    thinking how to do this time

---
+++ Programs/list-3/ex3-01.cpp
@@ -0,0 +1,18 @@
+/* One company decided to register all employees in one Computer System.
+You hired to develop this system. We will need to store the following employee data.
+
+NAME - name of  employee(max. of 40char)
+OFFICE - employee position(max. of 20char)
+SALARY - salary amount
+ADDRESS - address of employee(street, number, city, state, zip code)
+PHONE - contact phone of employee
+CODE - "true"  -> code available
+         "false" -> code in use    
+         
+The system must have the options for:
+A - include employee
+B - employee consultation by name or code
+C - change in employee data
+D - employee deletion
+E - employee report         */
+
